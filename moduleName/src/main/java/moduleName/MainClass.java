2997d89c00eb123a3d04fd77ef9d2b30039c4c22
pathakpra <prachipathak9876@gmail.com>
2024-04-20 11:15:29 +0530

    mgsss

---
+++ moduleName/src/main/java/moduleName/MainClass.java
@@ -0,0 +1,8 @@
+package moduleName;
+
+public class MainClass {
+
+	public void B() {
+		System.out.println("Done");
+	}
+}
---
+++ moduleName/src/test/java/jenkinProj/first.java
@@ -0,0 +1,12 @@
+package jenkinProj;
+
+import org.junit.jupiter.api.Test;
+
+public class first {
+
+	@Test
+	public void  A(){
+		System.out.println("HEllo");
+		
+	}
+}
